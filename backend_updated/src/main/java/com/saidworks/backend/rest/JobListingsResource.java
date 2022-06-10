334388508907515f30df00e1f98cd825f68b0ae3
Said ZITOUNI <zitouni.sd@gmail.com>
2022-06-10 15:50:19 -0400

    test preauthorize annotation on get and create

--- backend_updated/src/main/java/com/saidworks/backend/rest/JobListingsResource.java
+++ backend_updated/src/main/java/com/saidworks/backend/rest/JobListingsResource.java
@@ -8,9 +8,11 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:4200")
+
 @RestController
 @RequestMapping(value = "/api/jobListingss", produces = MediaType.APPLICATION_JSON_VALUE)
 public class JobListingsResource {
@@ -20,7 +22,7 @@ public class JobListingsResource {
     public JobListingsResource(final JobListingsService jobListingsService) {
         this.jobListingsService = jobListingsService;
     }
-
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @GetMapping
     public ResponseEntity<List<JobListingsDTO>> getAllJobListingss() {
         return ResponseEntity.ok(jobListingsService.findAll());
@@ -30,7 +32,7 @@ public class JobListingsResource {
     public ResponseEntity<JobListingsDTO> getJobListings(@PathVariable final Long id) {
         return ResponseEntity.ok(jobListingsService.get(id));
     }
-
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     @PostMapping
     @ApiResponse(responseCode = "201")
     public ResponseEntity<Long> createJobListings(
