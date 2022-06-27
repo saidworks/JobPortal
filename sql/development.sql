5134b2d25fb74d5318f000498715d2d12f7e2b09
Said ZITOUNI <zitouni.sd@gmail.com>
2022-06-27 19:52:50 -0400

    sql with basics commands used during development and ER Diagram

---
+++ sql/development.sql
@@ -0,0 +1,17 @@
+
+-- ALTER TABLE `jobportalonprogress`.`resume`
+-- DROP COLUMN user_id; 
+
+
+
+-- INSERT INTO `jobportalonprogress`.`role`
+-- (`id`,
+-- `role_name`)
+-- VALUES
+-- (2,
+-- "ROLE_ADMIN");
+--  INSERT INTO `jobportalonprogress`.`role`
+--  (`id`,
+-- `role_name`)
+-- VALUES(1,
+-- "ROLE_USER")
